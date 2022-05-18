5fc8d49fc9ebd6571e4eeddffa76242959826529
anon278 <105708297+anon278@users.noreply.github.com>
2022-05-19 01:28:50 +0200

    clean auto_api

--- Website/Express/auto_api.js
+++ Website/Express/auto_api.js
@@ -2,21 +2,18 @@
 const config = require("./api.json")
 
 module.exports = (app,db) => {
-    app.post(config[1]["route"], function(req,res) {
+        app.post(config[1]["route"], function(req,res) {
+            
        if(req.body.password == "password"){
             params = []
             for (let j in config[1]["params"][0]){
-                console.log("j is : ",j)
-                console.log(req.body[config[1]["params"][0][j]])
                 params.push(req.body[config[1]["params"][0][j]])
-            }
-            console.log("params is :",params)
-        
-            db.query(config[1]["query"], params
-            )
+            }        
+            db.query(config[1]["query"], params)
             res.send('successfully send')
 
         }else{
             res.send('Api failed')
         }
-    })}
+    })
+}
