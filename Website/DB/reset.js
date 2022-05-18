7e3a4d3189a84ca31440ce4db910d46fe1f6016d
anon278 <105708297+anon278@users.noreply.github.com>
2022-05-18 15:24:35 +0200

    pool sql.js / change name

--- Website/DB/reset.js
+++ Website/DB/reset.js
@@ -1,40 +1,26 @@
 const path = require('path');
 
 require('dotenv').config({path:path.join(__dirname,"../.env")});
+const db = require('./poolConnect.js');
 
 (async () => {
 
     //programm to delete, create and fill all the tables in the db
-    
-    const {Client} = require('pg')
-    const client = new Client({
-        "user": process.env.User,
-        "host": process.env.HOST,
-        "database": process.env.DATABASE,
-        "password": String(process.env.PASSWORD),
-        "port": process.env.PORT,
-      });
-    
-    
     const fs = require("fs");
-
-    client.connect();
 
     const get_table_query = `
         SELECT TABLE_NAME FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_TYPE = 'BASE TABLE' AND TABLE_CATALOG='Diamonds';
     `;
 
-    await (await client.query(get_table_query)).rows.map(x => x.table_name).filter(x => {
+    await (await db.query(get_table_query)).rows.map(x => x.table_name).filter(x => {
         return !(x.startsWith("pg_") || x.startsWith("sql_"))
     }).forEach(async (x) => {
         console.log(x);
-        await client.query(`DROP TABLE IF EXISTS ${x};`)
+        await db.query(`DROP TABLE IF EXISTS ${x};`)
     })
     const path = require("path")
 
     let sql = fs.readFileSync(path.join(__dirname,'tables.sql'), "utf8");
-    await client.query(sql);
-    
-    client.end();
+    await db.query(sql);
 
 })()
